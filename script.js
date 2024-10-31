0577800078882e7a560619a64662aa352d4d3c09
M0H1K4N <nejer911@gmail.com>
2024-10-31 23:13:02 +0400

    Add

---
+++ script.js
@@ -0,0 +1,83 @@
+
+
+
+
+// // aritmetical
+// console.log(6 + 5);
+// console.log(6 - 5);
+// console.log(6 * 5);
+// console.log(6 / 5);
+// console.log(6 % 5);
+
+
+// // comparison
+// console.log(6 == 5);
+// console.log(6 > 5);
+// console.log(6 < 5);
+// console.log(6 >= 5);
+// console.log(6 <= 5);
+// console.log(6 != 5);
+
+// // minicheba
+// let x = 5;
+// let y = 6;
+
+// console.log(x += y);
+// console.log(x -= y);
+// console.log(x /= y);
+// console.log(x %= y);
+
+
+// // bulean
+// const isEmptyPocket = true;
+// const moneyCame = false;
+
+// console.log(isEmptyPocket && moneyCame);
+// console.log(isEmptyPocket || moneyCame);
+// console.log(!(isEmptyPocket || moneyCame));
+// ////
+
+
+// function getInput() {
+//     const input = document.getElementById('userInput').value;
+//     document.getElementById('output').innerText = "You entered: " + input;
+// }
+// const userInput = prompt("Enter something:");
+// const alertinio = alert("You entered: " + userInput);
+
+
+
+// let str = "abrakadabrakadabrakadabrakadabrakadabrakad"
+// console.log(str.charAt(2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// logical operators
+// &&
+console.log( true && 100 );
+console.log( false && 100 );
+console.log( false || 100 );
+console.log( false && 100 );
+
+
+
+function onClick(){
+   prompt("type something: ");
+
+}
+
+// const testSomething = prompt("type something: ");
+// const userInput = alert("You entered: " + testSomething);
+
+
+
